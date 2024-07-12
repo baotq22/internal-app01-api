79ceb93d1fb41ca1dd9fd4debb4fc44270e6afe5
baotq22 <87086420+baotq22@users.noreply.github.com>
2024-07-12 22:01:35 +0700

    Add mongoose, express, oauth2 and successfully to connect with api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core"
+import { AppModule } from "./app.module"
+import { ValidationPipe } from "@nestjs/common"
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule)
+    const PORT = 3001
+
+    app.useGlobalPipes(new ValidationPipe())
+    app.enableCors()
+
+    await app.listen(PORT, () => {
+        console.log(`^^Connect Successfully on port ${PORT}`)
+    })
 }
-bootstrap();
+bootstrap()
