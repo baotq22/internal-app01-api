d6a03d27fbdd98ffd57b5c201ecfe91b91bc34b1
baotq22 <87086420+baotq22@users.noreply.github.com>
2024-07-13 22:47:45 +0700

    Set-up Mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from "@nestjs/common"
 import { AppController } from "./app.controller"
 import { AppService } from "./app.service"
+import { ConfigModule } from "@nestjs/config"
+import { MongooseModule } from "@nestjs/mongoose"
+import { MulterModule } from "@nestjs/platform-express"
+import { MemoryStoredFile, NestjsFormDataModule } from "nestjs-form-data"
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: ".env",
+            isGlobal: true
+        }),
+        MulterModule.register({
+            dest: "./uploads"
+        }),
+        NestjsFormDataModule.config({ storage: MemoryStoredFile }),
+        MongooseModule.forRoot(process.env.DB_URI),
+    ],
     controllers: [AppController],
     providers: [AppService]
 })
