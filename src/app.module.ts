1340ad7b49ab99c0415528c7d5ce6d24c6801c0e
baotq22 <87086420+baotq22@users.noreply.github.com>
2024-07-21 23:53:26 +0700

    Update for Image Handler

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,22 @@ import { ConfigModule } from "@nestjs/config"
 import { MongooseModule } from "@nestjs/mongoose"
 import { MemoryStoredFile, NestjsFormDataModule } from "nestjs-form-data"
 import { AuthModule } from "./auth/auth.module"
+import { ServeStaticModule } from "@nestjs/serve-static"
+import { join } from "path"
+import { MulterModule } from "@nestjs/platform-express"
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             envFilePath: ".env",
             isGlobal: true
+        }),
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, "..", "public/uploads/images"),
+            serveRoot: "/images" // Endpoint để truy cập ảnh, có thể thay đổi theo ý muốn
+        }),
+        MulterModule.register({
+            dest: "./uploads"
         }),
         NestjsFormDataModule.config({ storage: MemoryStoredFile }),
         MongooseModule.forRoot(process.env.DB_URI),
